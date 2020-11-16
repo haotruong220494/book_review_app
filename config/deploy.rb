3781f28277aedce3fd7014f7c1220b9b5881f563
haotruong220494 <truongnhatnam003@gmail.com>
2020-11-16 16:01:26 +0700

    Config deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@github.com:haotruong220494/book_review_app.git"
 set :ssh_options, { :forward_agent => true }
 
 set :pty, true
-set :linked_files, %w(config/database.yml config/application.yml config/master.key)
+set :linked_files, %w(config/database.yml config/application.yml)
 set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads)
 set :keep_releases, 5
 set :rvm_type, :user
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :stage, :production
 set :rails_env, :production
 set :branch, "main"
-set :deploy_to, "/home/haotruong/deploy/book_review_app"
-server "13.212.212.215", :user => "haotruong", :roles => %w(app db web)
+set :deploy_to, "/var/www/book_review_app"
+server "13.212.1.50", :user => "haotruong", :roles => %w(app db web)
