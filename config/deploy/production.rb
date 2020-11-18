9ada6ed7c22c94c7248f969f7fd286f37f720bdf
haotruong220494 <truongnhatnam003@gmail.com>
2020-11-18 14:42:35 +0700

    config deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ set :stage, :production
 set :rails_env, :production
 set :branch, "main"
 set :deploy_to, "/var/www/book_review_app"
-server "13.212.1.50", :user => "haotruong", :roles => %w(app db web)
+server "13.212.26.38", :user => "haotruong", :roles => %w(app db web)
