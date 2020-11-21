6d83e404d509ff8de23d5ac76efc02eb68e98bb4
haotruong220494 <truongnhatnam003@gmail.com>
2020-11-21 18:01:44 +0700

    Lazy load

---
+++ app/assets/javascripts/client/common.js
@@ -0,0 +1,11 @@
+$(function(){
+	$("img.lazy-load").lazyload({
+      event: "scroll.lazyload",
+      effect: "fadeIn",
+  }).trigger("lazyload");
+
+  $("img.lazy-header").lazyload({
+      event: "lazyload",
+      effect: "fadeIn",
+  }).trigger("appear");
+});
