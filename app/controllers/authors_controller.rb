cdfef435b6083570c881d1ba669a725a557b402f
haotruong220494 <truongnhatnam003@gmail.com>
2020-11-29 22:34:39 +0700

    page home

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,5 +3,7 @@ class AuthorsController < ApplicationController
 
   def show
     @author = Author.find_by_id params[:slug].delete("^0-9").to_i
+    @q = @author.posts.ransack params[:q]
+    @posts = @q.result.page(params[:page]).per(Settings.per_page.account)
   end
 end
