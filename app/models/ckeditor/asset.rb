0f0720436dec5f572686de9fbe4fb87ae6ca49a7
haotruong220494 <truongnhatnam003@gmail.com>
2020-12-03 17:22:03 +0700

    Add image activestorage

--- app/models/ckeditor/asset.rb
+++ app/models/ckeditor/asset.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
+require 'active_storage'
 
 class Ckeditor::Asset < ActiveRecord::Base
   include Ckeditor::Orm::ActiveRecord::AssetBase
   include Ckeditor::Backend::ActiveStorage
+  include Ckeditor::ActiveStorage
 
   attr_accessor :data
 
--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -2,7 +2,6 @@
 
 class Ckeditor::AttachmentFile < Ckeditor::Asset
   # for validation, see https://github.com/igorkasyanchuk/active_storage_validations
-
   def url_thumb
     @url_thumb ||= Ckeditor::Utils.filethumb(filename)
   end
