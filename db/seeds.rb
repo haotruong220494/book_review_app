1fc79f6e6330e0347e8db2a3a96cff40b72d24b7
haotruong220494 <truongnhatnam003@gmail.com>
2020-12-03 13:42:24 +0700

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,24 @@
 Admin.create(:email => "haotv03@gmail.com", :password => "hoanganhq12",
   :password_confirmation => "hoanganhq12")
+
+# 20.times do |index|
+#   Post.create(account_id: 1, category_id: 20, author_id: 3, name: "Những đứa trẻ #{index}", description: "Những đứa trẻ", slug: "nhung-dua-tre",
+#     content: "Chuyện Con Mèo Dạy Hải Âu Bay với nội dung được viết rất nhẹ nhàng và đơn giản: một con mèo đen mập ú hứa hẹn với một cô hải âu sắp mất rằng mình sẽ không ăn thịt, nhận trách nhiệm nuôi lớn và dạy cho con của cô biết bay. 
+
+# Một câu chuyện cổ tích, những người có đầu óc lý trí đều tin vào một bữa “trứng chiên ngon lành” dành cho chú mèo. Nhưng với Zorba, đó lại là một câu chuyện nghiêm túc, một lời hứa bằng vàng. Chú mèo đó luôn giữ tâm niệm “Ta đã hứa với mẹ con rồi.”
+
+# Thật ra, bắt nguồn của mọi lời hứa, chính là sự tin tưởng. Vào những giây phút cuối đời, Kengah may mắn gặp được chú mèo Zorba. Zorba, trong thời khắc đó, lại sẵn sàng tìm cách giúp đỡ một sinh vật hoàn toàn xa lạ với mình. Niềm tin luôn được xây dựng từ những điều nhỏ bé như thế.
+
+# Nhìn lại thực tế trong cuộc sống, lời hứa giữa con người vốn dĩ cũng rất trong sáng. Có thể trong giây phút lời hứa được cất lên, đã thật sự tồn tại lòng tin giữa các cá thể. Chỉ tiếc rằng sau đó lại mất đi cái bản lĩnh thực hiện chúng.
+
+# Từ ba lời hứa của cá nhân đi đến một lời thề của cả tập thể
+# Sau cái chết của Kengah, Zorba hoàn toàn có thể “thất hứa”. Và những nhân vật mèo khác như Đại Tá, Einstein, Secretario chẳng cần phải dính dáng đến một con hải âu còn chưa kịp ra đời. 
+
+# Nhưng lòng tự tôn của một con mèo cảng biển không cho phép chúng làm như thế. Và bằng sức mạnh tập thể cùng tình yêu thương vô điều kiện, chúng đã hoàn thành ước nguyện dang dở của Kengah.
+
+# Biết cách trân trọng ước nguyện người khác, cũng đã làm đẹp thêm cái tâm ở bản thân.
+
+# “Lời thề của một con mèo ở cảng cũng là lời thề của mọi con mèo khác.”
+
+# Từ ba lời hứa đi đến câu thề. Thật ra phải cần rất nhiều sự can đảm.")
+# end
