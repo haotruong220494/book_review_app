a6944af88f46baffe8663ff12ef26476a050479f
haotruong220494 <truongnhatnam003@gmail.com>
2020-12-06 20:58:33 +0700

    home page done

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,5 +3,6 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find_by_id params[:slug].delete("^0-9").to_i
+    @post_highlight = Category.cate_show("highlight").last.posts.order(created_at: :desc).limit(3)
   end
 end
