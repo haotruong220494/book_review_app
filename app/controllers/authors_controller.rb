39389e08a49f93a9641da1d2660eabb10f345ebe
haotruong220494 <truongnhatnam003@gmail.com>
2020-12-08 11:07:11 +0700

    tool seo

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -5,5 +5,7 @@ class AuthorsController < ApplicationController
     @author = Author.find_by_id params[:slug].delete("^0-9").to_i
     @q = @author.posts.ransack params[:q]
     @posts = @q.result.page(params[:page]).per(Settings.per_page.account)
+
+    @post_highlight = Category.cate_show("highlight").last.posts.order(created_at: :desc).limit(5)
   end
 end
