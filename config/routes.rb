51ecabd2a6532d623f5923d765d29e5c3770f57c
haotruong220494 <truongnhatnam003@gmail.com>
2020-12-08 11:57:56 +0700

    site map

--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,8 @@ Rails.application.routes.draw do
   root "client#home"
   get "/faq", to: "faqs#faq"
   get "/about-us", to: "abouts#about_us"
+  get "/about-us", to: "abouts#about_us"
+  get "/sitemap.xml", to: "sitemap#index", defaults: {format: "xml"}
   match "/404", to: "errors#file_not_found", via: :all
   match "/422", to: "errors#unprocessable", via: :all
   match "/500", to: "errors#internal_server_error", via: :all
